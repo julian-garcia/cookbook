00fa1b2c8d67f9f9fa58d549249a9e5f1c3daa01
Julian Garcia <julian.garcia.leoni@gmail.com>
2018-07-11 00:47:46 +0100

    Document ready added

--- static/scripts/chart.js
+++ static/scripts/chart.js
@@ -1,18 +1,20 @@
-// Render horizontal barchart for most popular recipes
-var ndx = crossfilter(popular_recipes);
-var dim = ndx.dimension(dc.pluck('recipe_title'));
-var grp = dim.group().reduceSum(function(d) {
-            return d.user_count;
-          });
+$(document).ready(function() {
+  // Render horizontal barchart for most popular recipes
+  var ndx = crossfilter(popular_recipes);
+  var dim = ndx.dimension(dc.pluck('recipe_title'));
+  var grp = dim.group().reduceSum(function(d) {
+              return d.user_count;
+            });
 
-dc.rowChart('#popular-graph')
-  .width(null)
-  .height(300)
-  .dimension(dim)
-  .group(grp)
-  .x(d3.scaleOrdinal())
-  .elasticX(true)
-  .margins({top: 0, right: 0, bottom: -1, left: 0})
-  .transitionDuration(500);
+  dc.rowChart('#popular-graph')
+    .width(null)
+    .height(300)
+    .dimension(dim)
+    .group(grp)
+    .x(d3.scaleOrdinal())
+    .elasticX(true)
+    .margins({top: 0, right: 0, bottom: -1, left: 0})
+    .transitionDuration(500);
 
-dc.renderAll();
+  dc.renderAll();
+});
